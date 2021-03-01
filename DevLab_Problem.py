46f8fdbcfd3fe179f9978ad215cfa321853dd4ff
SoraweeKan <65585968+SoraweeKan@users.noreply.github.com>
2021-03-01 12:03:25 +0700

    Create DevLab_Problem.py

---
+++ DevLab_Problem.py
@@ -0,0 +1,6 @@
+def gradeCalculate(num): #คำนวณเกรด
+    if (num >= 80 and num <= 100): print("A")
+    if (num >= 70 and num <= 79): print("B")
+    if (num >= 60 and num <= 69): print("C")
+    if (num >= 50 and num <= 59): print("D")
+    if (num < 50): print("F")
